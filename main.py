4483f79159dc250e4a4dfbd492c354e7666b47cb
jcates22 <catesjl9394@yahoo.com>
2023-04-24 21:53:37 -0400

    Add Fantasy 5 Game

--- main.py
+++ main.py
@@ -14,17 +14,27 @@ def lottery():
     num4p = random.randrange(1, 69)
     num5p = random.randrange(1, 69)
 
+    num1f = random.randrange(1, 42)
+    num2f = random.randrange(1, 42)
+    num3f = random.randrange(1, 42)
+    num4f = random.randrange(1, 42)
+    num5f = random.randrange(1, 42)
+
     mb = random.randrange(1, 25)
     pb = random.randrange(1, 26)
 
     result_m = (sorted([num1m, num2m, num3m, num4m, num5m]))
     result_p = (sorted([num1p, num2p, num3p, num4p, num5p]))
+    result_f = (sorted([num1f, num2f, num3f, num4f, num5f]))
 
-    game = input('Are you playing Mega Millions or Powerball?: ')
+    game = input('Are you playing Fantasy 5, Mega Millions, or Powerball?: ')
     print()
     game = game.lower()
 
-    if game.startswith('m'):
+    if game.startswith('f'):
+        print('Random number between 1 and 42')
+        print(result_f)
+    elif game.startswith('m'):
         print('Random number between 1 and 70')
         print(result_m)
         print()
